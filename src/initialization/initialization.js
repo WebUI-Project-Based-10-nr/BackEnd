79d1d5bf9ea563395ad042a3959d371822a56eeb
Annaivv <annashalya@gmail.com>
2024-10-11 01:27:06 +0300

    swagger connection added to initialization.js

--- src/initialization/initialization.js
+++ src/initialization/initialization.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Space2Study project API',
+      version: '1.0.0'
+    },
+    servers: [
+      {
+        url: 'http://localhost:8080'
+      }
+    ]
+  },
+  apis: ['./src/routes/*.js']
+}
+
+const swaggerSpec = swaggerJSDoc(options)
 
 const {
   config: { CLIENT_URL }
@@ -21,6 +41,10 @@ const initialization = (app) => {
       allowedHeaders: 'Content-Type, Authorization'
     })
   )
+
+  app.set('swaggerOptions', options)
+
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
   app.use('/', router)
 
