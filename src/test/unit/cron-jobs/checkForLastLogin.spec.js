552da4a08b0c7f40af7f39f8763eb245bb3bf520
Dubno Petro <petro.dubno@gmail.com>
2024-10-18 09:06:30 +0300

    fix checkForLastUserLogin

--- src/test/unit/cron-jobs/checkForLastLogin.spec.js
+++ src/test/unit/cron-jobs/checkForLastLogin.spec.js
@@ -27,8 +27,9 @@ describe('checkForLastUserLogin cron-job', () => {
   beforeEach(() => {
     mockedUsersList = { items: [{ ...mockedUser, lastLogin: mockedLastLoginDateToSendEmail }] }
     userService.getUsers = jest.fn(() => mockedUsersList)
-    jest.spyOn(global.Date, 'now').mockImplementation(() => new Date(2023, 7, 23, 25, 0, 0, 0).getTime());
-
+    jest.useRealTimers(); // Очищення попередніх налаштувань таймерів
+    const mockedCurrentDate = new Date(2023, 7, 23, 25, 0, 0, 0);
+    jest.useFakeTimers('legacy').setSystemTime(mockedCurrentDate);
   })
 
   afterEach(() => {
