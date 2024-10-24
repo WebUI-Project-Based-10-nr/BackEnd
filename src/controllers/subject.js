2d003729849ab5a6ed83aaa4e5735db6368860ed
Kateryna Bartienieva <k7bart@gmail.com>
2024-10-24 12:09:43 +0300

    add check if the category is passed

--- src/controllers/subject.js
+++ src/controllers/subject.js
@@ -4,6 +4,10 @@ const subjectService = require('~/services/subject')
 const getNamesByCategoryId = async (req, res) => {
   const { id: category } = req.params
 
+  if (!category) {
+    return res.status(400).json({ error: 'Category ID is required' })
+  }
+
   const match = getMatchOptions({ category })
 
   const names = await subjectService.getNamesByCategoryId(match)
