eb77a44b949d580bf4c5855d7f817a448d87b9ca
Kateryna Bartienieva <k7bart@gmail.com>
2024-10-28 14:22:28 +0200

    minor update

--- src/models/category.js
+++ src/models/category.js
@@ -2,18 +2,12 @@ const { Schema, model } = require('mongoose')
 const { FIELD_CANNOT_BE_EMPTY } = require('~/consts/errors')
 const { CATEGORY } = require('~/consts/models')
 
-const categorySchema = new Schema(
-  {
-    name: {
-      type: String,
-      unique: true,
-      required: [true, FIELD_CANNOT_BE_EMPTY('name')]
-    }
-  },
-  {
-    timestamps: true,
-    versionKey: false
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, FIELD_CANNOT_BE_EMPTY('name')]
   }
-)
+})
 
 module.exports = model(CATEGORY, categorySchema)
--- src/models/subject.js
+++ src/models/subject.js
@@ -2,21 +2,18 @@ const { Schema, model } = require('mongoose')
 const { FIELD_CANNOT_BE_EMPTY } = require('~/consts/errors')
 const { CATEGORY, SUBJECT } = require('~/consts/models')
 
-const subjectSchema = new Schema(
-  {
-    name: {
-      type: String,
-      unique: true,
-      required: [true, FIELD_CANNOT_BE_EMPTY('name')]
-    },
-    category: {
-      type: Schema.Types.ObjectId,
-      ref: CATEGORY,
-      required: [true, FIELD_CANNOT_BE_EMPTY('category')]
-    }
+const subjectSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, FIELD_CANNOT_BE_EMPTY('name')]
   },
-  { timestamps: true, versionKey: false }
-)
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: CATEGORY,
+    required: [true, FIELD_CANNOT_BE_EMPTY('category')]
+  }
+})
 
 subjectSchema.index({ name: 1 }, { unique: true })
 
