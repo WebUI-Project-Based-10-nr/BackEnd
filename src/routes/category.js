b718175b4fb65a40a42f33f2fdffab55ca8fcb67
Kateryna Bartienieva <k7bart@gmail.com>
2024-10-28 13:21:23 +0200

    update the Swagger docs

--- src/routes/category.js
+++ src/routes/category.js
@@ -7,7 +7,35 @@ const subjectRouter = require('~/routes/subject')
 
 const params = [{ model: Category, idName: 'id' }]
 
-router.use('/:id?/subjects', isEntityValid({ params }), subjectRouter)
+router.use('/:id/subjects', isEntityValid({ params }), subjectRouter)
+
+/**
+ * @swagger
+ * /categories/names:
+ *   get:
+ *     summary: Finds and returns an array with a list of categories
+ *     tags:
+ *       - category
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ *             example:
+ *               - _id: "66bdcae3f0d7edf34088ed1d"
+ *                 name: "Time Management"
+ *               - _id: "66bd1977da1a3f609fe9a1af"
+ *                 name: "Language"
+ */
 router.get('/names', asyncWrapper(categoryController.getCategoriesNames))
 
 module.exports = router
