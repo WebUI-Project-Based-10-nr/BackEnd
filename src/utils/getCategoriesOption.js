3f669a3eb0b354a62c34edb9fd0acf4496fb5099
Plotkin <Plotkin2016@gmail.com>
2024-10-29 14:34:49 +0200

    change getCategoriesOptions function

--- src/utils/getCategoriesOption.js
+++ src/utils/getCategoriesOption.js
@@ -1,7 +1,9 @@
 const getCategoriesOptions = (categories) => {
   if (Array.isArray(categories)) {
     return categories.map((item) => (item === 'null' ? null : item))
-  } else if (typeof categories === 'string') {
+  }
+
+  if (typeof categories === 'string') {
     return categories === 'null' ? [null] : [categories]
   }
 }
