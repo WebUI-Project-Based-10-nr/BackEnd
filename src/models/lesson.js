c6ec474ba456733bf8d589942660d7e036d1ef94
Plotkin <Plotkin2016@gmail.com>
2024-11-07 14:17:11 +0200

    changed lessonSchema and added lesson-schema

--- src/models/lesson.js
+++ src/models/lesson.js
@@ -1,14 +1,10 @@
 const { Schema, model } = require('mongoose')
-const { LESSON, USER, RESOURCES_CATEGORY, ATTACHMENT } = require('~/consts/models')
+const { enums } = require('~/consts/validation')
+const { LESSON, USER, RESOURCES_CATEGORY } = require('~/consts/models')
 const { FIELD_CANNOT_BE_EMPTY, FIELD_CANNOT_BE_LONGER, FIELD_CANNOT_BE_SHORTER } = require('~/consts/errors')
 
 const lessonSchema = new Schema(
   {
-    author: {
-      type: Schema.Types.ObjectId,
-      ref: USER,
-      required: [true, FIELD_CANNOT_BE_EMPTY('author')]
-    },
     title: {
       type: String,
       required: [true, FIELD_CANNOT_BE_EMPTY('title')],
@@ -19,23 +15,27 @@ const lessonSchema = new Schema(
       type: String,
       required: [true, FIELD_CANNOT_BE_EMPTY('description')],
       minLength: [1, FIELD_CANNOT_BE_SHORTER('description', 1)],
-      maxLength: [100, FIELD_CANNOT_BE_LONGER('description', 100)]
+      maxLength: [100, FIELD_CANNOT_BE_LONGER('description', 1000)]
     },
-    text: {
+    content: {
       type: String,
-      required: [true, FIELD_CANNOT_BE_EMPTY('text')],
-      minLength: [1, FIELD_CANNOT_BE_SHORTER('text', 1)],
-      maxLength: [100, FIELD_CANNOT_BE_LONGER('text', 100)]
+      required: [true, FIELD_CANNOT_BE_EMPTY('content')],
+      minLength: [50, FIELD_CANNOT_BE_SHORTER('content', 50)]
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: USER,
+      required: [true, FIELD_CANNOT_BE_EMPTY('author')]
     },
     category: {
       type: Schema.Types.ObjectId,
       ref: RESOURCES_CATEGORY,
       default: null
     },
-    attachments: {
-      type: [Schema.Types.ObjectId],
-      ref: ATTACHMENT,
-      default: []
+    resourceType: {
+      type: String,
+      enum: enums.RESOURCES_TYPES_ENUM,
+      default: enums.RESOURCES_TYPES_ENUM[0]
     }
   },
   { timestamps: true, versionKey: false }
