0eb3b3a2950cb2cc758aaf094dcd177b4edb627f
Annaivv <annashalya@gmail.com>
2024-11-14 16:36:33 +0200

    unit tests added

--- src/test/unit/services/quiz.spec.js
+++ src/test/unit/services/quiz.spec.js
@@ -10,7 +10,9 @@ jest.mock('~/models/quiz', () => ({
   lean: jest.fn().mockReturnThis(),
   populate: jest.fn().mockReturnThis(),
   exec: jest.fn(),
-  countDocuments: jest.fn()
+  countDocuments: jest.fn(),
+  findById: jest.fn(),
+  save: jest.fn()
 }))
 
 const mockItems = [{ title: 'Quiz 1' }, { title: 'Quiz 2' }]
@@ -77,5 +79,80 @@ describe('getQuizzes Service', () => {
 
     await expect(quizService.getQuizzes({}, {})).rejects.toThrow(errorMessage)
     expect(Quiz.find).toHaveBeenCalled()
+  })
+})
+
+describe('updateQuiz service', () => {
+  const mockAuthorId = '12345'
+  const mockQuizId = '6789'
+  const mockData = { title: 'Updated Title', description: 'Updated Description' }
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should update the quiz if it exists and belongs to the indicated author', async () => {
+    const mockQuiz = {
+      _id: mockQuizId,
+      author: mockAuthorId,
+      title: 'Initial title',
+      description: 'Initial description',
+      save: jest.fn().mockResolvedValue(true)
+    }
+
+    Quiz.findById.mockResolvedValue(mockQuiz)
+
+    await quizService.updateQuiz(mockAuthorId, mockQuizId, mockData)
+
+    expect(Quiz.findById).toHaveBeenCalledWith(mockQuizId)
+    expect(mockQuiz.title).toBe('Updated Title')
+    expect(mockQuiz.description).toBe('Updated Description')
+    expect(mockQuiz.save).toHaveBeenCalled()
+  })
+
+  it('should throw a not found error if the quiz does not exist', async () => {
+    const errorMessage = 'The requested URL was not found'
+
+    Quiz.findById.mockResolvedValue(null)
+
+    await expect(quizService.updateQuiz(mockAuthorId, mockQuizId, mockData)).rejects.toThrow(errorMessage)
+    expect(Quiz.findById).toHaveBeenCalledWith(mockQuizId)
+  })
+
+  it('should throw a not authorized error if the quiz has a different author', async () => {
+    const errorMessage = 'You do not have permission to perform this action.'
+    const mockQuiz = {
+      _id: mockQuizId,
+      author: 'Different author ID',
+      title: 'Initial title',
+      description: 'Initial description',
+      save: jest.fn().mockResolvedValue(true)
+    }
+
+    Quiz.findById.mockResolvedValue(mockQuiz)
+
+    await expect(quizService.updateQuiz(mockAuthorId, mockQuizId, mockData)).rejects.toThrow(errorMessage)
+    expect(Quiz.findById).toHaveBeenCalledWith(mockQuizId)
+  })
+
+  it('should update allowed fields only', async () => {
+    const mockQuiz = {
+      _id: mockQuizId,
+      author: mockAuthorId,
+      title: 'Initial title',
+      description: 'Initial description',
+      extraField: 'Not to be updated',
+      save: jest.fn().mockResolvedValue(true)
+    }
+
+    Quiz.findById.mockResolvedValue(mockQuiz)
+
+    const dataWithExtraField = { title: 'New title', extraField: 'Updated extra field' }
+
+    await quizService.updateQuiz(mockAuthorId, mockQuizId, dataWithExtraField)
+
+    expect(mockQuiz.title).toBe('New title')
+    expect(mockQuiz.extraField).toBe('Not to be updated')
+    expect(mockQuiz.save).toHaveBeenCalled()
   })
 })
